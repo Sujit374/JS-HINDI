d7ea5498cb5c3c9b918913a22c54308d7d19b517
Sujit kumar yadav <sky9955045@gmail.com>
2026-09-17 17:11:58 +0000

    dataType

---
+++ .devcontainer/01_basics/02_dataType.js
@@ -0,0 +1,25 @@
+"use strict";//treat all JS code as a new version
+
+//alert(3 + 3)// we are using nodejs,not browser
+
+console.log(3 + 3)
+
+console.log("Sujit")
+
+let name = "Sujit"
+let age = 20
+let isLoggedIN = false
+
+//number=>2 to power 53
+//string=>""
+//bigint 
+//boolean=>true/false
+//null=> standlone value
+//undefined
+//symbol=>unique
+
+
+//object
+
+console.log(typeof undifined);//undifined
+console.log(typeof null);//object
